3152336f73ec4eee09e19da8b90c9a65e45f5307
OysteinKoder <oystein.kodehode@gmail.com>
2024-01-08 19:13:59 +0100

    basic clickgame functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
-import { Button, H1, H2, P } from "@dnb/eufemia";
+import { Button, H1, H2, P, Space } from "@dnb/eufemia";
+import { useState } from "react";
 
 function App() {
+  const [score, setScore] = useState(0);
+  const [scoreAmount, setScoreAmount] = useState(1);
+  const [upgradeCost, setUpgradeCost] = useState(10);
+
+  const upgradeCostText = "kjøp for " + upgradeCost + ",- kr";
   return (
     <>
-      <div className="container">
+      <div
+        className="container"
+        onClick={() => {
+          setScore((score) => score + scoreAmount);
+        }}
+      >
         <H1>Spare-Spillet</H1>
-        <P size="large">Score: </P>
+        <P size="large">kr: {score} ,- </P>
+        <P>Klikk for å tjene penger!</P>
+        <Space top="x-large" />
+        <H2>Oppgradderinger</H2>
+        <p>Få en til krone for hvert klikk!</p>
+        <Button
+          text={upgradeCostText}
+          onClick={() => {
+            if (score >= upgradeCost) {
+              setScore((score) => score - upgradeCost);
+              setScoreAmount((scoreAmount) => scoreAmount + 1);
+              setUpgradeCost((upgradeCost) => upgradeCost * 2);
+            }
+          }}
+        />
       </div>
     </>
   );
