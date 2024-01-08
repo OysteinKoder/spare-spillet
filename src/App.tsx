8e759fb8d1fee41dfae4e5b19bc26cd29f35573e
OysteinKoder <oystein.kodehode@gmail.com>
2024-01-08 17:52:51 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button, H1, H2, P } from "@dnb/eufemia";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <H1>Spare-Spillet</H1>
+        <P size="large">Score: </P>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import "@dnb/eufemia/style/basis";
+import "@dnb/eufemia/style/components";
+import "@dnb/eufemia/style/themes/ui";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
