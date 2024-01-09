70f1b4ea8268c029decad6f92fec709cdb2b7aaf
OysteinKoder <oystein.kodehode@gmail.com>
2024-01-09 11:08:10 +0100

    Added a tab function as navigation.

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,20 @@
-import { Button, H1, H2, P, Space } from "@dnb/eufemia";
-import { useState } from "react";
-import { useSpring, animated } from "react-spring";
-import { FaDollarSign } from "react-icons/fa";
-
-const FallingDollar = () => {
-  const props = useSpring({
-    from: { top: -100, left: Math.random() * window.innerWidth },
-    to: { top: window.innerHeight },
-    config: { duration: 2000 },
-  });
-  return (
-    <animated.div style={{ ...props, position: "absolute", left: "50%" }}>
-      <FaDollarSign />
-    </animated.div>
-  );
-};
+import { H1, Tabs } from "@dnb/eufemia";
+import ClickerGame from "./components/clickerGame";
 
 function App() {
-  const [score, setScore] = useState(0);
-  const [scoreAmount, setScoreAmount] = useState(1);
-  const [upgradeCost, setUpgradeCost] = useState(10);
-  const [showDollar, setShowDollar] = useState(false);
-
-  const upgradeCostText = "kjøp for " + upgradeCost + ",- kr";
-
-  const triggerAnimation = () => {
-    setShowDollar(true);
-    setTimeout(() => setShowDollar(false), 2000);
-  };
-
   return (
     <>
       <div className="container">
-        <div
-          onClick={() => {
-            setScore((score) => score + scoreAmount);
-            triggerAnimation();
-          }}
-        >
-          <H1>Spare-Spillet</H1>
-          <P size="large">kr: {score} ,- </P>
-          <P>Klikk for å tjene penger!</P>
-          <Space top="x-large" />
-          <H2>Oppgradderinger</H2>
-          <p>Få en til krone for hvert klikk!</p>
-        </div>
-        <Button
-          text={upgradeCostText}
-          onClick={() => {
-            if (score >= upgradeCost) {
-              setScore((score) => score - upgradeCost);
-              setScoreAmount((scoreAmount) => scoreAmount + 1);
-              setUpgradeCost((upgradeCost) => Math.floor(upgradeCost * 1.5));
-            }
-          }}
-        />
-        <p>Dobble inntjening per klikk!</p>
-        <Button
-          text={upgradeCostText}
-          onClick={() => {
-            if (score >= upgradeCost) {
-              setScore((score) => score - upgradeCost);
-              setScoreAmount((scoreAmount) => scoreAmount * 2);
-              setUpgradeCost((upgradeCost) => Math.floor(upgradeCost * 3));
-            }
-          }}
-        />
-        {showDollar && <FallingDollar />}
+        <H1>Spare-Spillet</H1>
+
+        <Tabs>
+          <Tabs.Content title="Klikke-spill">
+            <ClickerGame />
+          </Tabs.Content>
+          <Tabs.Content title="Invisterings-spill">
+            <ClickerGame />
+          </Tabs.Content>
+        </Tabs>
       </div>
     </>
   );
---
+++ src/components/clickerGame.tsx
@@ -0,0 +1,81 @@
+import { Button, H2, P, Space } from "@dnb/eufemia";
+import { useState } from "react";
+import { useSpring, animated } from "react-spring";
+import { FaDollarSign } from "react-icons/fa";
+
+// Triggers a dollar that falls from the top of the screen
+// Is used whenever the user clicks the screen to earn money
+
+const FallingDollar = () => {
+  const props = useSpring({
+    from: { top: -100, left: Math.random() * window.innerWidth },
+    to: { top: window.innerHeight },
+    config: { duration: 2000 },
+  });
+  return (
+    <animated.div style={{ ...props, position: "absolute", left: "50%" }}>
+      <FaDollarSign />
+    </animated.div>
+  );
+};
+
+// Contains the whole clicker game
+
+function ClickerGame() {
+  const [score, setScore] = useState(0);
+  const [scoreAmount, setScoreAmount] = useState(1);
+  const [upgradeCost, setUpgradeCost] = useState(10);
+  const [showDollar, setShowDollar] = useState(false);
+
+  const upgradeCostText = "kjøp for " + upgradeCost + ",- kr";
+
+  // triggers the dollar animation
+  const triggerAnimation = () => {
+    setShowDollar(true);
+    setTimeout(() => setShowDollar(false), 2000);
+  };
+
+  return (
+    <>
+      <div
+        className="center"
+        onClick={() => {
+          setScore((score) => score + scoreAmount);
+          triggerAnimation();
+        }}
+      >
+        <P size="large">kr: {score} ,- </P>
+        <P>Klikk for å tjene penger!</P>
+        <Space top="10rem" />
+        <H2>Oppgradderinger</H2>
+        <p>Få en til krone for hvert klikk!</p>
+      </div>
+      <div className="center">
+        <Button
+          text={upgradeCostText}
+          onClick={() => {
+            if (score >= upgradeCost) {
+              setScore((score) => score - upgradeCost);
+              setScoreAmount((scoreAmount) => scoreAmount + 1);
+              setUpgradeCost((upgradeCost) => Math.floor(upgradeCost * 1.5));
+            }
+          }}
+        />
+        <p>Dobble inntjening per klikk!</p>
+        <Button
+          text={upgradeCostText}
+          onClick={() => {
+            if (score >= upgradeCost) {
+              setScore((score) => score - upgradeCost);
+              setScoreAmount((scoreAmount) => scoreAmount * 2);
+              setUpgradeCost((upgradeCost) => Math.floor(upgradeCost * 3));
+            }
+          }}
+        />
+        {showDollar && <FallingDollar />}
+      </div>
+    </>
+  );
+}
+
+export default ClickerGame;
