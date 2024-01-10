1cef9714e8624845f0ef5b21cc68dcb6ad54c479
OysteinKoder <oystein.kodehode@gmail.com>
2024-01-10 10:20:13 +0100

    added documentation comments

--- src/components/investGame.tsx
+++ src/components/investGame.tsx
@@ -2,6 +2,10 @@ import { P, Slider, Button, H2, Space, ProgressIndicator } from "@dnb/eufemia";
 import { useState, useEffect } from "react";
 import { companyMapping } from "./investGame/companyMapping";
 
+// To make the game more fun the prices for the companies are based on crypto prices,
+// Instead of the actual stock prices. This is because the stock prices are too stable.
+// This makes the prices fluctuate a lot, and makes the game more interesting
+
 function InvestGame() {
   const [data, setData] = useState([]);
 
@@ -9,6 +13,9 @@ function InvestGame() {
     console.log("buying");
   };
 
+  // if you need to use new properties from the API, you can add them here
+  // check out this link to see all properties
+  // https://api.coingecko.com/api/v3/coins/markets?vs_currency=nok
   interface Coin {
     current_price: number;
     price_change_24h: number;
@@ -16,9 +23,9 @@ function InvestGame() {
     id: string;
     name: string;
     image: string;
-    // include any other properties that a coin object might have
   }
 
+  // Fetches data from the coingecko API
   useEffect(() => {
     fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=nok")
       .then((response) => response.json())
@@ -26,6 +33,7 @@ function InvestGame() {
       .catch((error) => console.log(error));
   }, []);
 
+  // If the data has been fetched, it displays the stocks and buy option
   if (data[2]) {
     console.log(data);
     return (
@@ -64,7 +72,9 @@ function InvestGame() {
         })}
       </div>
     );
-  } else {
+  }
+  // If the data has not been fetched, it displays a loading screen
+  else {
     return (
       <div>
         <h1>Aksjer</h1>
